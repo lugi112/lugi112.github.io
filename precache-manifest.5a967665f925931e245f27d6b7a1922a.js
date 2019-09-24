512a95f138b56f473ca702a20a8249cde1cc8b78
lugi <39945603+lugi@users.noreply.github.com>
2019-09-24 06:24:05 +0000

    Deploying to master from master e3043cdac624e2d1ea5ef70e9f4f79a767664964

---
+++ precache-manifest.5a967665f925931e245f27d6b7a1922a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f85739d1ced3ef4db14d6cc65ba8928",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d94a9c202c00cdd7a178",
+    "url": "/static/css/main.19393e92.chunk.css"
+  },
+  {
+    "revision": "7684408bd5554961719d",
+    "url": "/static/js/2.4f057907.chunk.js"
+  },
+  {
+    "revision": "d94a9c202c00cdd7a178",
+    "url": "/static/js/main.f5cfbbd5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
